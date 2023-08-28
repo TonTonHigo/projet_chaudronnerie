ba0a0f9ccbcacda49b6b15318ce650014d36cc23
TonTonHigo <hugoh420@outlook.fr>
2023-08-28 14:30:37 +0400

    google fonts lien rajouté

--- Index.php
+++ Index.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page d'introduction</title>
     
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+    
     <!-- fichier css -->
     <link rel="stylesheet" href="style.css">
 </head>
