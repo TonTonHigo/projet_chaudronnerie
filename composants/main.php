2897b245a94213ea06e2d2261c119e7ac12bb146
TonTonHigo <hugoh420@outlook.fr>
2023-08-28 16:21:58 +0400

    main

---
+++ composants/main.php
@@ -0,0 +1,19 @@
+<main>
+
+    <section id="propos">
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor expedita dolorem nobis tenetur ad impedit sequi quas aliquam unde eligendi quaerat vero commodi totam, quo eum inventore sit quam quisquam!</p>
+        </div>
+    </section>
+
+</main>
