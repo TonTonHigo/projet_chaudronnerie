f6d54700b6dc92ceb3f67cf2fe18ba478091169f
TonTonHigo <hugoh420@outlook.fr>
2023-08-28 09:54:36 +0400

    ajout dossier dp et .sql de la bdd

---
+++ chaudronnerie.sql
@@ -0,0 +1,65 @@
+CREATE DATABASE bdd_chaudronnerie;
+USE bdd_chaudronnerie;
+
+CREATE TABLE `Role` (
+	`id_role` INT NOT NULL AUTO_INCREMENT,
+	`type` varchar(20) NOT NULL,
+	PRIMARY KEY (`id_role`)
+);
+
+CREATE TABLE `Article` (
+	`id_article` INT NOT NULL AUTO_INCREMENT,
+	`titre` INT NOT NULL,
+	`contenu` INT NOT NULL,
+	`image` INT NOT NULL,
+	PRIMARY KEY (`id_article`)
+);
+
+CREATE TABLE `Galerie` (
+	`id_galerie` INT NOT NULL AUTO_INCREMENT,
+	`photo` varchar(100) NOT NULL,
+	`descriptif` TEXT(1000) NOT NULL,
+	PRIMARY KEY (`id_galerie`)
+);
+
+CREATE TABLE `Contact` (
+	`id_contact` INT NOT NULL AUTO_INCREMENT,
+	`pseudonyme` varchar(30) NOT NULL,
+	`email` varchar(30) NOT NULL,
+	`sujet` varchar(50) NOT NULL,
+	`message` TEXT(5000) NOT NULL,
+	`id_utilisateur` INT NOT NULL,
+	PRIMARY KEY (`id_contact`)
+);
+
+CREATE TABLE `Utilisateur` (
+	`id_utilisateur` INT NOT NULL AUTO_INCREMENT,
+	`pseudonyme` varchar(30) NOT NULL,
+	`email` varchar(30) NOT NULL,
+	`mdp` varchar(100) NOT NULL,
+	`id_role` INT NOT NULL,
+	PRIMARY KEY (`id_utilisateur`)
+);
+
+CREATE TABLE `Commentaire` (
+	`id_commentaire` INT NOT NULL AUTO_INCREMENT,
+	`message` INT NOT NULL,
+	`id_article` INT NOT NULL,
+	`id_utilisateur` INT NOT NULL,
+	PRIMARY KEY (`id_commentaire`)
+);
+
+ALTER TABLE `Contact` ADD CONSTRAINT `Contact_fk0` FOREIGN KEY (`id_utilisateur`) REFERENCES `Utilisateur`(`id_utilisateur`);
+
+ALTER TABLE `Utilisateur` ADD CONSTRAINT `Utilisateur_fk0` FOREIGN KEY (`id_role`) REFERENCES `Role`(`id_role`);
+
+ALTER TABLE `Commentaire` ADD CONSTRAINT `Commentaire_fk0` FOREIGN KEY (`id_article`) REFERENCES `Article`(`id_article`);
+
+ALTER TABLE `Commentaire` ADD CONSTRAINT `Commentaire_fk1` FOREIGN KEY (`id_utilisateur`) REFERENCES `Utilisateur`(`id_utilisateur`);
+
+
+
+
+
+
+
