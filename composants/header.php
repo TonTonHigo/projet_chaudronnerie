b4d848a69b7fe12aceaa3e99d87f266494ea46c6
Pitt <piikme7@gmail.com>
2023-08-28 17:15:22 +0200

    Up

--- composants/header.php
+++ composants/header.php
@@ -1,7 +1,7 @@
 <header>
     <div class="navbar">
         <div class="logo">
-            <a href="#"><img src="image\Logo.png" alt="Logo"></a>
+            <a href="introduction.php"><img src="image\Logo.png" alt="Logo"></a>
         </div>
         <div class="centered-links">
             <a href="archive.php">Archive</a>
@@ -9,7 +9,7 @@
             <a href="galerie.php">Galerie</a>
         </div>
         <div class="contact">
-            <a href="#">Contact</a>
+            <a href="formulaire.php">Contact</a>
         </div>
     </div>
 </header>
