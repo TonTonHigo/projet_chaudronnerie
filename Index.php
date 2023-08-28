6922b3cc70328a721eb0172498e25c10c929cecb
TonTonHigo <hugoh420@outlook.fr>
2023-08-28 14:36:42 +0400

    ajout jquery

--- Index.php
+++ Index.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
-    
+
     <!-- fichier css -->
     <link rel="stylesheet" href="style.css">
 </head>
@@ -26,6 +26,7 @@
 ?>
   
   <!-- fichier js -->
+  <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
   <script src="script.js"></script>
 </body>
 </html>
