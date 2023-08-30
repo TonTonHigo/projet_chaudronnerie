5d93d9877a4251b788d17774239ea24658484ceb
TonTonHigo <hugoh420@outlook.fr>
2023-08-30 10:48:15 +0400

    redirection nav good

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){ 
+   
+    var url = window.location.pathname.split('/').pop();
+
+    if(url = 'index.php'){
+        $('#logo').attr('src','image/Logo.png');
+        $('#lien_index').attr('href', 'index.php');
+        $('#lien_archive').attr('href', 'pages/archive.php');
+        $('#lien_tutoriel').attr('href', 'pages/tutoriel.php');
+        $('#lien_galerie').attr('href', 'pages/galerie.php');
+        $('#lien_formulaire').attr('href', 'pages/formulaire.php');
+    }
+
+ 
+        
     $('.logowow').hover(function(){
         $(this).animate({width:'400px'},400);
     },function(){
@@ -16,4 +30,5 @@ $(document).ready(function(){
         $('.preload').fadeOut('slow');
         $('.containerlogo').fadeOut('slow');
     });
+
 });
