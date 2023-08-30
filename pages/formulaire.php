a458c926b45c8579ea840331c8f10f98ec312da0
TonTonHigo <hugoh420@outlook.fr>
2023-08-30 09:36:29 +0400

    liens css et script sur les pages good

---
+++ pages/formulaire.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contactez-moi</title>
+
+     <!-- google fonts -->
+     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+
+    <!-- fichier css -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   
+<?php include "../composants/header.php" ?>
+
+<main>
+    
+</main>
+
+
+<?php include "../composants/footer.php" ?>
+
+ <!-- fichier js -->
+ <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="script.js"></script>
+</body>
+</html>
--- pages/galerie.php
+++ pages/galerie.php
@@ -2,7 +2,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Galerie</title>
+
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+
+    <!-- fichier css -->
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
@@ -15,5 +23,8 @@
 
 <?php include "../composants/footer.php" ?>
     
+ <!-- fichier js -->
+ <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+  <script src="script.js"></script>
 </body>
 </html>
