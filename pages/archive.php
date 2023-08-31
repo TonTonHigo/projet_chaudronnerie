cf38cd696c69a045bab10d553c1bf3802a34d2fb
Pitt <piikme7@gmail.com>
2023-08-31 12:20:31 +0200

    Up

--- pages/archive.php
+++ pages/archive.php
@@ -11,7 +11,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <!-- fichier css -->
     <link rel="stylesheet" href="../style.css">
 
@@ -23,87 +23,56 @@
 
     <main>
 
-        <?php
+        <div class="container-sandy">
+            <div class="fond-sandy">
+                <div class="grid-galerie">
+                    <?php
+                    $archive = $connexion->select("*", "article");
+                    foreach ($archive as $cartes) {
+                        $maxContentLength = 100; // Maximum de caractère a afficher 
 
-        $articles = $connexion->select("*", "article");
+                        // Raccourci le contenu trop long pour donner un effet prévu
+                        $truncatedContent = (strlen($cartes['contenu']) > $maxContentLength) ?
+                            substr($cartes['contenu'], 0, $maxContentLength) . "..." :
+                            $cartes['contenu'];
 
-        if (!empty($articles)) {
-            foreach ($articles as $article) {
-                echo '<div class="image-container" onclick="showArticleDetails(this)">
-                            <h2>' . $article["titre"] . '</h2>
-                            <p>' . $article["contenu"] . '</p>
-                            <img src="' . $article["image"] . '">
-                          </div>';
-            }
-        } else {
-            echo "Aucun article trouvé.";
-        }
-         ?>
-        <!--
-        <div class="image-grid image-grid-highlight">
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image13.jpg" alt="Image 1">
+                        echo '                   
+                        
+                    <div class="cadre-photo" style="background-image: url(' . $cartes["image"] . ')">
+                        <h2 id="text-galerie' . $cartes["id_article"] . '" class="text-galerie">' . $truncatedContent . '</h2>
+                    </div>                 
+                    ';
+                    }
+                    ?>
+                </div>
             </div>
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image14.jpg" alt="Image 2">
-            </div>
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image15.jpg" alt="Image 3">
-            </div>
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image16.jpg" alt="Image 4">
-            </div>
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image18.png" alt="Image 5">
-            </div>
-            <div class="image-container" onclick="showArticleDetails(this)">
-                <img src="../image/image11a.jpg" alt="Image 6">
-            </div>
+
         </div>
-         -->
+
+
+
+
 
     </main>
 
     <?php include "../composants/footer.php" ?>
 
-    <!-- fichier js -->
+    <!-- fichier js  -->
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
     <script>
-        
-    // Fonction pour afficher les détails de l'article
-    function showArticleDetails(imageContainer) {
-        var title = imageContainer.getAttribute("data-title");
-        var content = imageContainer.getAttribute("data-content");
-        var imageSrc = imageContainer.querySelector("img").getAttribute("src");
+        $(document).ready(function() {
+            // galerie
+            $('.text-galerie').hide();
 
-        // Afficher les détails de l'article dans une boîte modale
-        var modalContent = '<h2>' + title + '</h2>' +
-            '<p>' + content + '</p>' +
-            '<img src="' + imageSrc + '" alt="Image de l\'article">';
-
-        // Créer une boîte modale et ajouter le contenu
-        var modal = document.createElement('div');
-        modal.className = 'modal';
-        modal.innerHTML = modalContent;
-
-        // Ajouter la boîte modale à la page
-        document.body.appendChild(modal);
-
-        // Ajouter un écouteur pour fermer la boîte modale lorsqu'on clique dessus
-        modal.addEventListener('click', function () {
-            document.body.removeChild(modal);
+            $('.cadre-photo').hover(function() {
+                $(this).find('.text-galerie').slideDown("slow");
+            }, function() {
+                $(this).find('.text-galerie').slideUp("slow");
+            });
         });
-    }
-
-    // Associer la fonction aux éléments d'article
-    $(".article.image-container").click(function () {
-        showArticleDetails(this);
-    });
-
-
     </script>
-
-
 </body>
 
 </html>
