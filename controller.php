dc1ee9be3262e655eda7686c8a708f9e367de434
TonTonHigo <hugoh420@outlook.fr>
2023-09-06 11:14:40 +0400

    ajout photo galerie good

--- controller.php
+++ controller.php
@@ -11,14 +11,12 @@ if (isset($_POST["form"])) {
                 $response = array('success' => false, 'message' => 'Failed to send message.');
             }
             break;
+
         case "galerie":
             $inserted = $connexion->insert_galerie($_POST["photo"], $_POST['descriptif']);
-            if ($inserted) {
-                $response = array('success' => true, 'message' => 'Message sent successfully.');
-            } else {
-                $response = array('success' => false, 'message' => 'Failed to send message.');
-            }
+            header('location: pages/galerie.php');
             break;
+
         default:
             $response = array('success' => false, 'message' => 'Invalid form type.');
             break;
