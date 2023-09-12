3e725cc214d0407a8293a5c7a8d5c4ddc658f6af
Pitt <piikme7@gmail.com>
2023-09-12 13:42:26 +0200

    Up

--- Index.php
+++ Index.php
@@ -6,16 +6,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Twin Multiservices</title>
 
-    <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <!-- bootstrap -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-    <!-- google fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
 
-    <!-- fichier css -->
-    <link rel="stylesheet" href="style.css">
+  <!-- fichier css -->
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -49,15 +49,16 @@
       </div>
       <div class="histoire">
         <p>
-          <strong>La Naissance de l'Art :</strong>
+          <strong>La Naissance de l'Art :</strong><br>
           La chaudronnerie, c'est l'art de donner vie au métal. Elle a vu le jour bien avant nos machines modernes, lorsque les chaudronniers utilisaient le feu, le marteau et l'enclume pour créer des objets magiques.
         </p>
         <p>
-          <strong>Le Métal en Transformation :</strong>
+          <strong>Le Métal en Transformation :</strong><br>
           La chaudronnerie, c'est la transformation du métal en quelque chose de plus grand, de plus utile, de plus beau. Chaque pièce forgée est une œuvre d'art en soi, une symphonie de formes et de textures métalliques.
         </p>
+        <br>
         <p>
-          <strong>L'Héritage du Passé :</strong>
+          <strong>L'Héritage du Passé :</strong><br>
           De génération en génération, cet art précieux a été transmis. Les secrets de la chaudronnerie, les techniques et le savoir-faire se sont perpétués.
         </p>
         <br>
@@ -66,13 +67,15 @@
         </p>
         <br>
         <p>
-          <strong>Un Art Vivant Aujourd'hui :</strong>
-          Aujourd'hui, la chaudronnerie est plus vivante que jamais. Les artisans contemporains repoussent les limites de la créativité en combinant tradition et modernité.
+          <strong>Un Art Vivant Aujourd'hui :</strong><br>
+          Le chaudronnier est un artisan qui maîtrise les techniques de traçage, de découpe, de mise en forme et d’assemblage des métaux en feuilles, des tubes et des profilés. Il réalise des pièces variées, allant du chaudron domestique au réservoir industriel, en passant par la charpente métallique, la tuyauterie ou le mobilier.
         </p>
+        <br>
         <p>
-          <strong>La Chaudronnerie, une Histoire Éternelle :</strong>
-          L'histoire de la chaudronnerie est une histoire éternelle, une célébration de la beauté du métal, de la créativité humaine et de la persévérance de l'artisan.
+          <strong>La Chaudronnerie :</strong><br>
+         Une branche industrielle qui regroupe plusieurs secteurs d’activité, tels que l’alimentaire, la chimie, l’énergie, l’aéronautique, la navale, l’automobile ou la menuiserie. La chaudronnerie s’adapte en permanence aux évolutions technologiques et aux besoins du marché. Elle utilise des matériaux divers, comme l’acier, l’inox, le cuivre, l’aluminium, le titane ou les matières plastiques.
         </p>
+        <br>
         <p>
           <a href=".\pages\formulaire.php" class="btn-contact">Message ici</a>
         </p>
