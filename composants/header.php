c8c984b4d6f4f0aa67c1ef0e542ec0760b446fc6
TonTonHigo <hugoh420@outlook.fr>
2023-09-13 09:31:14 +0400

    lien bootstrap manquant dans contact rajouté

--- composants/header.php
+++ composants/header.php
@@ -23,56 +23,56 @@
     </div>
 
     <!-- Modal connexion -->
-    <div class="modal fade" id="conn-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="crud forme-form" method="POST" action="../controller.php">
-                        <input class="form" type="hidden" name="form" value="connexion">
-                        <div class="validation-error pseudocoError">Pseudonyme manquant</div>
-                        <input type="text" id="pseudoco" name="pseudoco" placeholder="Pseudonyme">
-                        <div class="validation-error mdpError">Mdp manquant</div>
-                        <input type="password" id="mdpco" name="mdpco" placeholder="Mdp">
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn annuler" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn enregistrer">Enregistrer</button>
-                </div>
-            </div>
-        </div>
-    </div>   
-
-    <!-- Modal inscription -->
-    <div class="modal fade" id="inscri-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <form class="crud forme-form" method="POST" action="../controller.php">
-                    <input class="form" type="hidden" name="form" value="inscription">
-                        <div class="validation-error pseudoinsError">Pseudonyme manquant</div>
-                        <input type="text" id="pseudoins" name="pseudoins" placeholder="Pseudonyme">
-                        <div class="validation-error emailError">Email manquant</div>
-                        <input type="text" id="email" name="email" placeholder="Email">
-                        <div class="validation-error mdpinsError">Mdp manquant</div>
-                        <input type="password" id="mdpins" name="mdpins" placeholder="Mdp">
-                        <div class="validation-error confirmError">Mdp manquant</div>
-                        <input type="password" id="confirm" name="confirm" placeholder="Confirmer Mdp">
-                    </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn annuler" data-bs-dismiss="modal">Annuler</button>
-                    <button type="button" class="btn enregistrer">Enregistrer</button>
+        <div class="modal fade" id="conn-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form class="crud forme-form" method="POST" action="../controller.php">
+                            <input class="form" type="hidden" name="form" value="connexion">
+                            <div class="validation-error pseudocoError">Pseudonyme manquant</div>
+                            <input type="text" id="pseudoco" name="pseudoco" placeholder="Pseudonyme">
+                            <div class="validation-error mdpError">Mdp manquant</div>
+                            <input type="password" id="mdpco" name="mdpco" placeholder="Mdp">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn annuler" data-bs-dismiss="modal">Annuler</button>
+                        <button type="button" class="btn enregistrer">Enregistrer</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>    
+        </div>   
+    
+        <!-- Modal inscription -->
+        <div class="modal fade" id="inscri-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form class="crud forme-form" method="POST" action="../controller.php">
+                        <input class="form" type="hidden" name="form" value="inscription">
+                            <div class="validation-error pseudoinsError">Pseudonyme manquant</div>
+                            <input type="text" id="pseudoins" name="pseudoins" placeholder="Pseudonyme">
+                            <div class="validation-error emailError">Email manquant</div>
+                            <input type="text" id="email" name="email" placeholder="Email">
+                            <div class="validation-error mdpinsError">Mdp manquant</div>
+                            <input type="password" id="mdpins" name="mdpins" placeholder="Mdp">
+                            <div class="validation-error confirmError">Mdp manquant</div>
+                            <input type="password" id="confirm" name="confirm" placeholder="Confirmer Mdp">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn annuler" data-bs-dismiss="modal">Annuler</button>
+                        <button type="button" class="btn enregistrer">Enregistrer</button>
+                    </div>
+                </div>
+            </div>
+        </div>    
 </header>
--- pages/formulaire.php
+++ pages/formulaire.php
@@ -4,8 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contactez-moi</title>
 
-     <!-- google fonts -->
-     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
 
@@ -16,10 +19,10 @@
    
 <?php include "../composants/header.php" ?>
 
-<h1 id="titre-contact">Contactez-moi</h1>
 
 <main>
     <form id="contact_form">
+        <h1 id="titre-contact">Contactez-moi</h1>
         <div id="pseudonymeError" class="validation-error">Pseudonyme manquant</div>
         <input name="pseudonyme" id="pseudonyme" type="text" placeholder="Pseudonyme">
         
@@ -43,10 +46,12 @@
 <?php include "../composants/footer.php" ?>
 
 <!-- fichier js -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function () {
         $('#merci-contact').hide();
+        $('.validation-error').hide();
 
         $('#send_contact').click(function () {
             // Gather form data
