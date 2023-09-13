cd1ee12d7de040a1d2b92012ea622ff5b77a5d6f
Pitt <piikme7@gmail.com>
2023-09-13 07:40:31 +0200

    Up

--- pages/tutoriel.php
+++ pages/tutoriel.php
@@ -1,32 +1,37 @@
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tutoriel</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tutoriel</title>
 
-     <!-- google fonts -->
-     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+  <!-- google fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Wallpoet&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 
-    <!-- fichier css -->
-    <link rel="stylesheet" href="../style.css">
+  <!-- fichier css -->
+  <link rel="stylesheet" href="../style.css">
 </head>
+
 <body>
+
+  <?php include "../composants/header.php" ?>
+
+  <main>
     
-<?php include "../composants/header.php" ?>
 
-<main>
-    
-</main>
+  </main>
 
 
-<?php include "../composants/footer.php" ?>
+  <?php include "../composants/footer.php" ?>
 
- <!-- fichier js -->
+  <!-- fichier js -->
   <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
   <script>
 
   </script>
 </body>
+
 </html>
