2d5c92c52a56994b1538d23a0ee33d1a9beb066b
Pitt <piikme7@gmail.com>
2023-09-20 13:40:26 +0200

    Up

--- composants/header.php
+++ composants/header.php
@@ -27,7 +27,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Connexion</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
@@ -52,7 +52,7 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">Modification de photo</h1>
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Inscription</h1>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
